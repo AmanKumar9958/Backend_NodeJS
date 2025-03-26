8af85c448964631e0fc6ac2882e2213e377ee9a4
Aman Kumar <amancollege04@gmail.com>
2025-03-26 16:27:01 +0530

    Learned about linking CSS and JS file

--- Form/index.js
+++ Form/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+// rendering the HTML..
 app.set('view engine', 'ejs');
 
 // when using post method to send data to the server, we have to use these two middleware (builtin)..
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+// adding CSS file..
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
