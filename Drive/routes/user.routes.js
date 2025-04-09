d757a3c380528899f51f26fa1fa60bcdfc72380e
Aman Kumar <amancollege04@gmail.com>
2025-04-09 15:12:09 +0530

    Added validation on username, email and password

--- Drive/routes/user.routes.js
+++ Drive/routes/user.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// for validation..
+const { body, validationResult } = require('express-validator');
+
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -12,9 +15,21 @@ router.get('/register', (req, res) => {
 });
 
 // route for sending data to server..
-router.post('/register', (req, res) => {
-    console.log(req.body)
-    res.send('User registered successfully!');
+router.post('/register', 
+    // validating the data..
+    body('email').trim().isEmail().isLength({min: 13}).withMessage('Invalid email address'),
+    body('password').trim().isLength({ min: 5 }).withMessage('Password should be at least 5 characters long'),
+    body('username').trim().isLength({ min: 3 }).withMessage('Username should be at least 3 characters long'),
+    (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array(),
+            message: 'Invalid data',
+        })
+    } else{
+        res.send('User registered successfully');
+    }
 })
 
 
