006f45cbff8f106e474dc1afa823ec1a4775f9ce
Aman Kumar <amancollege04@gmail.com>
2025-04-11 14:00:26 +0530

    successfully uploaded the image in the Cloudniary database.

--- Drive/routes/index.routes.js
+++ Drive/routes/index.routes.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+// for file upload..
+const multer = require('multer');
+const { storage } = require('../config/cloudniary')
+const upload = multer({ storage });
+
 router.get('/home', (req, res) => {
     res.render('home');
 })
+
+router.post('/upload-file', upload.single('file'), (req, res) => {
+    if (!req.file) return res.status(400).send('No file uploaded');
+
+    res.json({
+        url: req.file.path,
+        fileName: req.file.originalname
+    });
+});
+
+
+
 
 module.exports = router;
